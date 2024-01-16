92347076ae90bd8dc95dcc8ee955b7905fa60355
LuisaFermino <souzafermino49@gmail.com>
2024-01-16 11:28:37 -0300

    Implementação do Layout do Menu Lateral

--- src/componentes/MenuLateral.js
+++ src/componentes/MenuLateral.js
@@ -1,10 +1,41 @@
 import styled from "styled-components";
 
+import { IoPeople } from "react-icons/io5";
+import { IoCheckmarkOutline } from "react-icons/io5";
+import { IoLockOpen } from "react-icons/io5";
+import { IoLockClosed } from "react-icons/io5";
+
 function MenuLateral({ setMenuVisivel }) {
   return (
     <Menu>
       <Esquerda onClick={() => setMenuVisivel(false)}></Esquerda>
-      <Direita></Direita>
+      <Direita>
+        <Titulo>
+          <p>Escolha um contato para enviar mensagem:</p>
+        </Titulo>
+        <div>
+          <Etapa>
+            <IoPeople />
+            <p>Todos</p>
+            <Check></Check>
+          </Etapa>
+        </div>
+        <Titulo>
+          <p>Escolha a visibilidade:</p>
+        </Titulo>
+        <div>
+          <Etapa>
+            <IoLockOpen />
+            <p>Publico</p>
+            <Check></Check>
+          </Etapa>
+          <Etapa>
+            <IoLockClosed></IoLockClosed>
+            <p>Reservadamente</p>
+            <Check></Check>
+          </Etapa>
+        </div>
+      </Direita>
     </Menu>
   );
 }
@@ -34,6 +65,41 @@ const Direita = styled.div`
   position: fixed;
   right: 0;
   top: 0;
+`;
+
+const Titulo = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2vh;
+
+  p {
+    text-align: center;
+    padding-top: 4vh;
+    font-size: 16px;
+    width: 55vw;
+    font-weight: 700;
+  }
+`;
+
+const Etapa = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+  margin-top: 2vh;
+
+  p {
+    font-size: 16px;
+    font-weight: 400;
+    margin-left: 3vw;
+  }
+`;
+
+const Check = styled(IoCheckmarkOutline)`
+  font-size: 24px;
+  z-index: 1000;
+  color: #28bb25;
+  position: fixed;
+  right: 5vw;
 `;
 
 export default MenuLateral;
